7d70e35ab95727c5ab83ef808b95e6dc913a0bd6
delandapp <alihandronikolas@gmail.com>
2024-04-25 05:29:35 +0700

    update

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,7 +1,11 @@
+import 'dart:convert';
+
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:form_validator/form_validator.dart';
 import 'package:get/get.dart';
 import 'package:readism/app/data/constans/endpoint.dart';
+import 'package:readism/app/data/models/response_bookSearch.dart';
 import 'package:readism/app/data/models/response_buku.dart';
 import 'package:readism/app/data/models/response_kategori.dart';
 import 'package:readism/app/data/provider/api_provider.dart';
@@ -14,8 +18,12 @@ class MyState<T1,T2> {
 
 class HomeController extends GetxController with StateMixin<MyState<List<DataKategori>, List<DataBuku>>>{
   //TODO: Implement HomeController
+    final validator = ValidationBuilder().minLength(1).build();
   final RxString username = StorageProvider.read(StorageKey.username).obs;
     final TextEditingController search = TextEditingController();
+    final RxInt searchLenght = 0.obs;
+    final RxBool loading = false.obs;
+    final RxList<DataBukuSearch> listDataBuku = <DataBukuSearch>[].obs;
 
   final count = 0.obs;
   @override
@@ -33,6 +41,52 @@ class HomeController extends GetxController with StateMixin<MyState<List<DataKat
   void onClose() {
     super.onClose();
   }
+
+   Future<void> searchData(String value) async {
+    try {
+      if (search.text.toString() == "") {
+        listDataBuku.value = [];
+        searchLenght.value = 0;
+        return;
+      }
+      String data = jsonEncode({
+        "query": search.text.toString(),
+      });
+      search.text.toString() != ""
+              ? searchLenght.value = 1
+              : searchLenght.value = 0;
+      loading.value = true;
+      final bearerToken = StorageProvider.read(StorageKey.bearerToken);
+      final response = await ApiProvider.instance().post(
+          "${Endpoint.book}/search",
+          options: Options(headers: {"Authorization": "Bearer $bearerToken"}),
+          data: data);
+      if (response.statusCode == 200) {
+        loading.value = false;
+        final ResponseBookSearch responseBook =
+            ResponseBookSearch.fromJson(response.data);
+        if (responseBook.data!.isEmpty) {
+          listDataBuku.value = [];
+        } else {
+          listDataBuku.value = responseBook.data!;
+        }
+      } else {
+        change(null, status: RxStatus.error("Gagal mengambil data"));
+      }
+    } on DioException catch (e) {
+      if (e.response != null) {
+        if (e.response?.data != null) {
+          change(null,
+              status: RxStatus.error("${e.response?.data['message']}"));
+        }
+      } else {
+        change(null, status: RxStatus.error(e.message ?? ""));
+      }
+    } catch (e) {
+      change(null, status: RxStatus.error(e.toString()));
+    }
+  }
+
 
    Future<void> getData() async{
     change(null, status: RxStatus.loading());
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -3,10 +3,12 @@ import 'package:flutter/services.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import 'package:get/get.dart';
+import 'package:readism/app/components/bookSearch.dart';
 import 'package:readism/app/components/buildSection.dart';
 import 'package:readism/app/components/kategory.dart';
 import 'package:readism/app/components/myInput.dart';
 import 'package:readism/app/components/navProfil.dart';
+import 'package:readism/app/components/searchInput.dart';
 
 import '../controllers/home_controller.dart';
 
@@ -16,74 +18,90 @@ class HomeView extends GetView<HomeController> {
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark.copyWith(
       statusBarColor: Colors.black,
-      statusBarIconBrightness: Brightness.light,// Change this color as needed
+      statusBarIconBrightness: Brightness.light, // Change this color as needed
     ));
     final heightFullBody =
         MediaQuery.of(context).size.height - MediaQuery.of(context).padding.top;
     final widthFullBody = MediaQuery.of(context).size.width;
     return Scaffold(
-      
       body: Container(
         color: Colors.black,
         width: widthFullBody,
         height: heightFullBody,
         child: controller.obx(
-          (state) => SingleChildScrollView(
-            child: Container(
-              width: widthFullBody,
-              decoration: const BoxDecoration(
-                  image: DecorationImage(
-                      image: AssetImage('assets/images/back.png'), fit: BoxFit.fill)),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    width: widthFullBody,
-                    decoration: const BoxDecoration(
-                        image: DecorationImage(
-                            image: AssetImage('assets/images/bg-nav.png'),
-                            fit: BoxFit.fill)),
-                    child: navProfil(
-                      title: "Hi !",
-                      subtitle: controller.username.value.toString(),
+          (state) => Container(
+            width: widthFullBody,
+            height: heightFullBody,
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/images/back.png'),
+                    fit: BoxFit.fill)),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  width: widthFullBody,
+                  decoration: const BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage('assets/images/bg-nav.png'),
+                          fit: BoxFit.fill)),
+                  child: navProfil(
+                    title: "Hi !",
+                    subtitle: controller.username.value.toString(),
+                  ),
+                ),
+                SizedBox(height: heightFullBody * 0.02),
+                Center(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 2,
+                          blurRadius: 7,
+                          offset: const Offset(
+                              0, 3), // changes position of shadow
+                        ),
+                      ],
+                    ),
+                    width: widthFullBody * 0.9,
+                    child: MyInputSearch(
+                      prefixIcon: Icons.search,
+                      validator: controller.validator,
+                      controller: controller,
+                      controllerField: controller.search,
+                      height: 20,
+                      width: 10,
+                      hintText: "Search Your Favorit Book",
+                      autoFocus: false,
                     ),
                   ),
-                  SizedBox(height: heightFullBody * 0.02),
-                  Center(
-                    child: Container(
-                        decoration: BoxDecoration(
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.5),
-                              spreadRadius: 2,
-                              blurRadius: 7,
-                              offset: const Offset(
-                                  0, 3), // changes position of shadow
+                ),
+                SizedBox(height: heightFullBody * 0.02),
+                MyKategori(
+                  data: state!.state1!,
+                ),
+                Obx(
+                  () => Expanded(
+                    child: controller.searchLenght.value == 1
+                        ? controller.loading.value == false
+                            ? controller.listDataBuku.isEmpty
+                                ? const Center(child: Text("Tidak Ada Buku"))
+                                : BookSearch(data: controller.listDataBuku)
+                            : const Center(child: CircularProgressIndicator())
+                        : SingleChildScrollView(
+                          child: Column(
+                              children: state.state2!.map((data) {
+                                return SizedBox(
+                                  height: heightFullBody * 0.3,
+                                  child: buildSection(data: data),
+                                );
+                              }).toList(),
                             ),
-                          ],
                         ),
-                        width: widthFullBody * 0.9,
-                        child: myInput(
-                          controller: controller,
-                          controllerField: controller.search,
-                          hintText: "Search",
-                          autoFocus: true,
-                          textAlign: TextAlign.center,
-                          prefixIcon: FontAwesomeIcons.searchengin,
-                        )),
                   ),
-                  SizedBox(height: heightFullBody * 0.02),
-                  MyKategori(data: state!.state1!,),
-                  Column(
-                    children: state.state2!.map((data) {
-                      return SizedBox(
-                        height: heightFullBody * 0.3,
-                        child: buildSection(data: data),
-                      );
-                    }).toList(),
-                  )
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ),
